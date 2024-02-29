39b9c295dd230996dfa80f5a11ed1722e153d11c
denad74 <denad.ramic@gmail.com>
2024-02-29 23:12:29 +0100

    Create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium  text-purple-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis unde
+        non hic omnis perferendis quos, inventore quam tenetur sapiente, vel
+        odio accusantium, quibusdam minus soluta. Porro dolores doloremque totam
+        quidem? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis unde
+        non hic omnis perferendis quos, inventore quam tenetur sapiente, vel
+        odio accusantium, quibusdam minus soluta. Porro dolores doloremque totam
+        quidem? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Repudiandae earum fuga debitis temporibus, eum id inventore vero
+        voluptate et veniam vel incidunt quam recusandae laboriosam alias
+        deserunt eaque reiciendis sequi!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis unde
+        non hic omnis perferendis quos, inventore quam tenetur sapiente, vel
+        odio accusantium, quibusdam minus soluta. Porro dolores doloremque totam
+        quidem? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Repudiandae earum fuga debitis temporibus, eum id inventore vero
+        voluptate et veniam vel incidunt quam recusandae laboriosam alias
+        deserunt eaque reiciendis sequi!
+      </p>
+    </div>
+  );
+}
+
+export default page;
