2b4db1eac4e9c607afcf3b45709b8782b9d3dd88
denad <denad@192.168.1.219>
2024-02-29 10:30:31 +0100

    instaling next js 13 and tailwind

---
+++ src/app/head.jsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMDb Clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="This is the IMDb clone with next js and tailwind"
+      />
+
+      <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+    </>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+// Fix the problem for "search params object is empty in production with next 13 app dir"
+
+// for more info "https://github.com/vercel/next.js/issues/43077"
+
+export const dynamic = "force-dynamic"; // this is the fix
+
+export default async function Home() {
+  return (
+    <div>
+      <h1>Hello </h1>
+    </div>
+  );
+}
