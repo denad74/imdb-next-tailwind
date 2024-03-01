7bf3e2cccb986e7cf1c534495921d5f030a7616d
denad74 <denad.ramic@gmail.com>
2024-03-01 15:50:21 +0100

    Fetch movie data from tmdb api to home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,30 @@
 // Fix the problem for "search params object is empty in production with next 13 app dir"
 
+import Results from "@/components/Results";
+
 // for more info "https://github.com/vercel/next.js/issues/43077"
-
+const API_KEY = process.env.API_KEY;
 export const dynamic = "force-dynamic"; // this is the fix
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const url = `https://api.themoviedb.org/3/${
+    genre === "fetchTrending" ? "movie/top_rated" : "trending/all/week"
+  }?api_key=${API_KEY}&language=en-US&page=1
+`;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTrending" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1
+    `,
+    { next: { revalidate: 100000 } }
+  );
+  if (!res.ok) throw new Error("Failed to fetch data");
+  const data = await res.json();
+  const results = data.results;
 
-export default async function Home() {
   return (
     <div>
-      <h1 className="text-red-400">Hello </h1>
+      <Results results={results} />
     </div>
   );
 }
